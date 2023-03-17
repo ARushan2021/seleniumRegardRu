4ee9feb5760198c84637b84a416010379079867f
ARushan@appline.ru <ARushan@appline.ru>
2023-03-17 18:46:57 +0300

    Создан проект seleniumRegardRu

---
+++ src/main/java/hooks/Hooks.java
@@ -0,0 +1,33 @@
+package hooks;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import static base.DriverManager.closeDriver;
+import static base.DriverManager.getWebDriver;
+
+public class Hooks {
+
+    final WebDriver driver = getWebDriver();
+
+    @After
+    public void tearDown(Scenario scenario) {
+        String screenshotName = scenario.getName().replace(" ", "_");
+        try {
+            if (scenario.isFailed()){
+                TakesScreenshot ts = (TakesScreenshot) driver;
+                byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
+                scenario.attach(screenshot, "image/png", screenshotName);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        closeDriver();
+
+    }
+
+}
