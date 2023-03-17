f9c60f13803ed90ef259218be3357ce2c27b84eb
ARushan@appline.ru <ARushan@appline.ru>
2023-03-18 01:52:54 +0300

    Добавил driver.close

--- src/main/java/hooks/Hooks.java
+++ src/main/java/hooks/Hooks.java
@@ -1,13 +1,11 @@
 package hooks;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-
-import java.util.concurrent.TimeUnit;
 
 import static base.DriverManager.closeDriver;
 import static base.DriverManager.getWebDriver;
@@ -29,6 +27,14 @@ public class Hooks {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
+    @AfterAll
+    public static void before_or_after_all() throws InterruptedException {
+        closeDriver();
+    }
+
+
+
 
 }
